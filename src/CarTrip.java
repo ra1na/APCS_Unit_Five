dff7b1d957d2f6b023766e8f938e90fd3ce9b000
rainaweinberg <raina.weinberg@ssfs.org>
2022-01-12 11:44:23 -0500

    cartrip

--- src/CarTrip.java
+++ src/CarTrip.java
@@ -1,3 +1,63 @@
 public class CarTrip {
+    int myStartOdometer;
+    int myEndOdometer;
+    double myTime;
+    double myGallonsUsed;
 
+
+    public CarTrip (int myStartOdometer, int myEndOdometer, double myTime, double myGallonsUsed) {
+        this.myStartOdometer = myStartOdometer;
+        this.myEndOdometer = myEndOdometer;
+        this.myTime = myTime;
+        this.myGallonsUsed = myGallonsUsed;
+    }
+
+    public int getMyStartOdometer () {
+        return myStartOdometer;
+    }
+
+    public int getMyEndOdometer() {
+        return myEndOdometer;
+    }
+
+    public double getMyTime() {
+        return myTime;
+    }
+
+    public double getMyGallonsUsed() {
+        return myGallonsUsed;
+    }
+
+    public void setMyStartOdometer (int myStartOdometer) {
+        this.myStartOdometer = myStartOdometer;
+    }
+
+    public void setMyEndOdometer(int myEndOdometer) {
+        this.myEndOdometer = myEndOdometer;
+    }
+
+    public void setMyTime(double myTime) {
+        this.myTime = myTime;
+    }
+
+    public void setMyGallonsUsed(double myGallonsUsed) {
+        this.myGallonsUsed = myGallonsUsed;
+    }
+
+    public int getTripDistance () {
+        return myEndOdometer - myStartOdometer;
+    }
+
+    public double getAverageSpeed () {
+        return getTripDistance()/myTime;
+    }
+
+    public double getGasMileage () {
+        return getTripDistance()/myGallonsUsed;
+    }
+
+    public String getTotalGasPrice (double pricePerGallon) {
+        int distanceTraveled = (int)(((myGallonsUsed * pricePerGallon) * 100) + 0.5);
+        return "Distance traveled: " + distanceTraveled + " miles";
+    }
 }
